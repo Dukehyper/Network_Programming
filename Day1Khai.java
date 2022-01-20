f5fd691ba54a8718d6b28664d382c6d4f099310f
xetryDcoder <xetry.d.coder@gmail.com>
2022-01-20 10:13:26 +0545

    try chatch deleted

--- Day1Khai.java
+++ Day1Khai.java
@@ -3,11 +3,6 @@ import java.net.*;
 
 public class Day1Khai {
     public static void main(String[] args) {
-        try {
-            InetAddress address = InetAddress.getByName("172.217.27.164");
-            System.out.println(address.getCanonicalHostName());
-        } catch (UnknownHostException e) {
-            System.out.println(e);
-        }
+       
     }
 }
